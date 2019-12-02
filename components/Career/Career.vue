718614f8f4ec5299a82cf23e09bc064128a02bba
Alexandre JORON <ajoron@production.apside.lan>
2019-12-02 09:54:41 +0100

    Creation of the Career component

---
+++ components/Career/Career.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="career">
+    <h1 class="title">Career</h1>
+    <div class="experiences-list">
+      <Experience
+        v-for="item in experiences"
+        :key="item.title"
+        :title="item.title"
+        :company="item.company"
+        :location="item.location"
+        :startDate="item.startDate"
+        :endDate="item.endDate"
+        :description="item.description"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Experience from "./Experience.vue";
+
+export default {
+  components: {
+    Experience
+  },
+  data() {
+    return {
+      name: "career",
+      experiences: [
+        {
+          title: "Full Stack Developer on Aurélia / C#",
+          company: "Apside / Berger Levrault",
+          location: "Toulouse, France",
+          startDate: "03/2019",
+          endDate: "09/2019",
+          description:
+            "Projet de fin d'étude (PFE) au sein de l'ESN Apside, en mission chez le client Berger-Levrault. Objectif : Développement d'une plateforme web médico-sociale.Environnement fonctionnel : concevoir, réaliser et recetter les solutions techniques à mettre en œuvre dans un des modules de la plateforme santé (Dossier administratif, Facturation, Dossier de soins, Planning, Prescription, Connecteur …)"
+        },
+        {
+          title:
+            "Project Manager, Integrator and Developer Full Stack JavaScript",
+          company: "Armatis-Lc",
+          location: "Calais, France",
+          startDate: "09/2018",
+          endDate: "03/2018",
+          description:
+            "Projet Innovation et Conception (PIC) réalisé au sein de l'entreprise Armatis-lc accompagné de deux collègues élèves-ingénieurs : Jean-Christophe Meyer et Ulysse Pirlot. Conception d'une solution digitale accompagnant le téléconseiller dans le but de simplifier sa tâche quotidienne et lui offrir des moyens d’agrémenter sa conversation avec le client en prenant appui sur les fichiers de prospection en base et en allant sourcer sur le web des informations pertinentes et géolocalisées. Environnement technique : Node.Js, HTML, CSS, Javascript, C#."
+        },
+        {
+          title: "Intern in Web development under AGILE method",
+          company: "U-Need Consulting",
+          location: "Toulouse, France",
+          startDate: "06/2018",
+          endDate: "08/2018",
+          description:
+            "Stage d'assistant ingénieur au sein de l'entreprise U-Need. Intervention sur l’évolution et l’optimisation d'une application web de gestion. Digitalisation du système de facturation de l’entreprise. Pilotage et réalisation du projet par la mise en place d'une méthode AGILE. Environnement technique : PHP, SQL, Javascript, JQuery, CSS, HTML."
+        },
+        {
+          title: "Integrator, designer and web developer",
+          company: "2ST-CI and Wake Up Africa",
+          location: "Abidjan, Côte d'Ivoire",
+          startDate: "06/2016",
+          endDate: "08/2016",
+          description:
+            "Stage au sein de l'ENS 2ST-CI et l'association Wake Up Africa, association créée par des ivoiriens dans le but de développer les domaines de la santé et de l'éducation en Côte d'Ivoire et, plus globalement, en Afrique. Développement d'une plateforme web d'échange de données médicales ayant pour objectif de faciliter les diagnostics médicaux et de les rendre accessibles dans tout le pays. Création du site officiel de l'association Wake Up Africa. Environnement technologique : CSS, HTML, PHP, MySQL, Bootstrap et jQuery."
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+$colour-primary: #11467f;
+$border-color: #cfdae5;
+
+.career {
+  background-color: $colour-primary;
+
+  .title {
+    text-align: center;
+    font-size: 80px;
+    color: #ffffff;
+    margin: 20px 0 50px 0; 
+    text-decoration: underline;
+  }
+
+  .experiences-list {
+    border-left: 9px solid $border-color;
+    margin: auto;
+    width: 55%;
+    padding-left: 71px;
+  }
+}
+</style>
---
+++ components/Career/Experience.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="experience">
+    <span class="circle" />
+    <div class="experience__title">{{ title }}</div>
+    <div class="experience__company">{{ company }} at {{ location }}</div>
+    <div class="experience__date">{{ startDate }} to {{ endDate }}</div>
+    <div class="experience__description">{{ description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Experience",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    company: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    startDate: {
+      type: String,
+      required: true
+    },
+    endDate: {
+      type: String,
+      default: "now"
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+$circle-size: 51px;
+$array-size: 12px;
+$colour-primary: #11467f;
+$back-color: #ffffff;
+$border-color: #cfdae5;
+
+.experience {
+  padding: 15px;
+  border-radius: 17px;
+  margin: 15px auto;
+  position: relative;
+  background-color: $back-color;
+  border: 9px solid $border-color;
+  box-shadow: 10px 5px 5px darken($colour-primary, 5%);
+  transition: transform 400ms;
+  transform-origin: -84px 50%;
+  &:hover {
+    transform: scale(1.04);
+  }
+
+  &__title {
+    font-size: 20px;
+    font-weight: bold;
+    color: $colour-primary;
+  }
+
+  &::after {
+    content: "";
+    height: 0;
+    width: 0;
+    display: block;
+    position: absolute;
+    top: 50%;
+    border-right: solid $array-size $back-color;
+    border-top: solid $array-size transparent;
+    border-bottom: solid $array-size transparent;
+    margin-top: -$array-size;
+    left: -$array-size;
+  }
+
+  &::before {
+    content: "";
+    height: 0;
+    width: 0;
+    display: block;
+    position: absolute;
+    top: 50%;
+    border-right: solid $array-size + 9 $border-color;
+    border-top: solid $array-size + 9 transparent;
+    border-bottom: solid $array-size + 9 transparent;
+    margin-top: -($array-size + 9);
+    left: -($array-size + 9);
+  }
+
+  .circle {
+    background-color: $back-color;
+    border: 9px solid $border-color;
+    width: $circle-size;
+    height: $circle-size;
+    border-radius: $circle-size / 2;
+    display: block;
+    position: absolute;
+    margin-top: -$circle-size / 2;
+    left: -84px - $circle-size / 2;
+    top: 50%;
+  }
+}
+</style>
